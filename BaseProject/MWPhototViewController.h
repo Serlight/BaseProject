50bbf5cf179c6eecb5ae218b6b18cf6991d8b3bf
wbx.hcc <hcc@winbaoxian.com>
2016-03-28 15:55:01 +0800

    新版新闻消息推送，

---
+++ BaseProject/MWPhototViewController.h
@@ -0,0 +1,13 @@
+//
+//  MWPhototViewController.h
+//  BaseProject
+//
+//  Created by 何长春 on 16/3/28.
+//  Copyright © 2016年 Serlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWPhototViewController : UIViewController
+
+@end
--- Pods/Target Support Files/Pods-BaseProject/Pods-BaseProject-resources.sh
+++ Pods/Target Support Files/Pods-BaseProject/Pods-BaseProject-resources.sh
@@ -59,9 +59,11 @@ install_resource()
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "JSPatch/JSPatch/JSPatch.js"
+  install_resource "${BUILT_PRODUCTS_DIR}/MWPhotoBrowser.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "JSPatch/JSPatch/JSPatch.js"
+  install_resource "${BUILT_PRODUCTS_DIR}/MWPhotoBrowser.bundle"
 fi
 
 mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
